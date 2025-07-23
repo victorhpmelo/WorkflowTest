f7dde5645cf88630fd62149d3b5864951398e978
Victor Melo <victorhpmelo01@hotmail.com>
2025-07-23 01:25:33 -0300

    Commit de criação do CRUD do projeto

---
+++ WorkflowTest/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkflowTest.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Person> People { get; set; }
+
+    }
+}
---
+++ WorkflowTest/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace WorkflowTest.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime BirthDate { get; set; }
+    }
+}
